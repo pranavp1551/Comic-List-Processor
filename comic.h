9eec61699255685b59f2c6d04374ba03cc409ff9
pranavp1551 <49173066+pranavp1551@users.noreply.github.com>
2023-06-26 13:07:09 -0400

    Update comic.h

--- comic.h
+++ comic.h
@@ -1,7 +1,4 @@
-/**
-“As a Hokie, I will conduct myself with honor and integrity at all times. 
-I will not lie, cheat, or steal, nor will I accept the actions of those who do.”*/
-//Pranav Poodari
+
 
 #include <stdio.h>
 #include <stdbool.h>
